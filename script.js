b524c9d0057eceb4c0ca9e48c60bed04ebc90b7e
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-09-26 02:09:29 +0300

    fix chromecast init issue

--- script.js
+++ script.js
@@ -40,11 +40,13 @@ const beginCast = ({videoUrl, subtitles = []}) => {
     castSession.loadMedia(request).
       then(() => console.log(`Load succeed`)).
       catch((errorCode) => console.log(`Error code: ${errorCode}`));
-  }, console.error);
+  }, (e) => {
+    console.error(e)
+  });
 
 };
 
-startCastButton.addEventListener(`click`, () => {
+startCastButton.addEventListener(`click`, (evt) => {
   const videoUrl = videoSourceUrl.value;
   if (!videoUrl) {
     console.error(`Video URL is not set`);
@@ -58,22 +60,56 @@ startCastButton.addEventListener(`click`, () => {
   }
 
   beginCast(info);
+
+  evt.stopPropagation();
+  evt.preventDefault();
 });
 
-const init = () => {
+const stopCasting = () => {
+  const castSession = getInstance().getCurrentSession();
+  // End the session and pass 'true' to indicate
+  // that receiver application should be stopped.
+  castSession.endSession(true);
+};
+
+const initOldWay = () => {
   getInstance().setOptions({
     receiverApplicationId: chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID,
-    autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED,
-    resumeSavedSession: true
+    autoJoinPolicy: chrome.cast.AutoJoinPolicy.ORIGIN_SCOPED
   });
 
-  const stopCasting = () => {
-    const castSession = getInstance().getCurrentSession();
-    // End the session and pass 'true' to indicate
-    // that receiver application should be stopped.
-    castSession.endSession(true);
-  };
-  stopAllButton.addEventListener(`click`, stopCasting);
+};
+
+const initNewWay = () => {
+  const sessionRequest = new chrome.cast.SessionRequest(chrome.cast.media.DEFAULT_MEDIA_RECEIVER_APP_ID);
+  const apiConfig = new chrome.cast.ApiConfig(sessionRequest,
+    (session) => {
+    },
+    (e) => {
+      if (e === 'available') {
+        console.log('receiver found');
+      }
+      else {
+        console.log('receiver list empty');
+      }
+    },
+    chrome.cast.AutoJoinPolicy.PAGE_SCOPED);
+
+  chrome.cast.initialize(apiConfig, () => console.log(`Inited successfully`), (e) => console.error(e));
+};
+
+
+const init = () => {
+  initNewWay();
+
+  stopAllButton.addEventListener(`click`, (evt) => {
+    evt.stopPropagation();
+    evt.preventDefault();
+
+    stopCasting();
+
+
+  });
 };
 
 window['__onGCastApiAvailable'] = function (isAvailable) {
