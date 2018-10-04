910c8db1b32a0fb99d77a22a6d311386d79d7b71
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-10-05 01:23:28 +0300

    update proxy

--- proxy.js
+++ proxy.js
@@ -21,32 +21,8 @@ proxy.on('connect', (req, cltSocket, head) => {
 });
 
 // now that proxy is running
-proxy.listen(1337, '127.0.0.1', () => {
-
-  // make a request to a tunneling proxy
-  const options = {
-    port: 1337,
-    hostname: '127.0.0.1',
-    method: 'CONNECT',
-    path: 'www.google.com:80'
-  };
-
-  const req = http.request(options);
-  req.end();
-
-  req.on('connect', (res, socket, head) => {
-    console.log('got connected!');
-
-    // make a request over an HTTP tunnel
-    socket.write('GET / HTTP/1.1\r\n' +
-      'Host: www.google.com:80\r\n' +
-      'Connection: close\r\n' +
-      '\r\n');
-    socket.on('data', (chunk) => {
-      console.log(chunk.toString());
-    });
-    socket.on('end', () => {
-      proxy.close();
-    });
-  });
+const port = 1337;
+const hostname = '127.0.0.1';
+proxy.listen(port, hostname, () => {
+  console.log(`Proxy is up and running on http://${hostname}:${port}`);
 });
