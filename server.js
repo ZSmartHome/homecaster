0fc925c42ac9e2fba3ab051f68b7f6953e6fd9ce
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-10-05 01:23:53 +0300

    support video hosting

--- server.js
+++ server.js
@@ -1,15 +1,33 @@
 const http = require('http');
 const util = require('util');
+const fs = require('fs');
+const open = util.promisify(fs.open);
+const path = require('path');
 
 const server = http.createServer();
 
 server.on(`request`, (req, res) => {
-  console.log(`got request to: ${req.url}`);
+  const url = req.url || ``;
+  console.log(`got request to: ${url}`);
   console.log(req.headers);
 
   res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World\n');
+  if (url.endsWith(`mp4`)) {
+    const filePath = path.join(__dirname, 'video/test.mp4');
+    const stat = fs.statSync(filePath);
+
+    res.writeHead(200, {
+      'Content-Type': 'video/mp4',
+      'Content-Length': stat.size
+    });
+
+    const readStream = fs.createReadStream(filePath);
+    // We replaced all the event handlers with a simple call to readStream.pipe()
+    readStream.pipe(res);
+  } else {
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+  }
 });
 
 const PORT = 8080;
