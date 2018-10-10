7e9c80c27850fdc5b83c9eaf92cbf1298ee5ff6f
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2018-10-11 00:37:02 +0300

    add subtitles example

--- server.js
+++ server.js
@@ -1,8 +1,8 @@
-const http = require('http');
-const util = require('util');
-const fs = require('fs');
+const http = require(`http`);
+const util = require(`util`);
+const fs = require(`fs`);
 const open = util.promisify(fs.open);
-const path = require('path');
+const path = require(`path`);
 
 const server = http.createServer();
 
@@ -17,20 +17,30 @@ server.on(`request`, (req, res) => {
   res.setHeader(`Access-Control-Allow-Headers`, `Content-Type, Accept`);
   res.setHeader(`Access-Control-Max-Age`, `3600`);
   if (url.endsWith(`mp4`)) {
-    const filePath = path.join(__dirname, 'video/test.mp4');
+    const filePath = path.join(__dirname, `video/test.mp4`);
     const stat = fs.statSync(filePath);
 
-    res.writeHead(200, {
-      'Content-Type': 'video/mp4',
-      'Content-Length': stat.size
-    });
+    res.setHeader(`Content-Type`, `video/mp4`);
+    res.setHeader(`Content-Length`, stat.size);
+    res.writeHead(200);
+
+    const readStream = fs.createReadStream(filePath);
+    // We replaced all the event handlers with a simple call to readStream.pipe()
+    readStream.pipe(res);
+  } else if (url.endsWith(`vtt`)) {
+    const filePath = path.join(__dirname, `video/video.vtt`);
+    const stat = fs.statSync(filePath);
+
+    res.setHeader(`Content-Type`, `text/vtt`);
+    res.setHeader(`Content-Length`, stat.size);
+    res.writeHead(200);
 
     const readStream = fs.createReadStream(filePath);
     // We replaced all the event handlers with a simple call to readStream.pipe()
     readStream.pipe(res);
   } else {
-    res.setHeader('Content-Type', 'text/plain');
-    res.end('Hello World\n');
+    res.setHeader(`Content-Type`, `text/plain`);
+    res.end(`Hello World\n`);
   }
 });
 
