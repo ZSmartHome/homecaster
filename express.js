e8bb4f32bb72cf101d360228b90526c7c86d9c79
Evgenii Shchepotev <evgenii.schepotiev@gmail.com>
2019-05-23 23:56:19 +0300

    add https handling

--- express.js
+++ express.js
@@ -2,6 +2,19 @@ const express = require(`express`);
 const app = express();
 const port = 8080;
 
-app.use(express.static(`www`));
+app.use(express.static(`./www`));
 
-app.listen(port, () => console.log(`App is ready at http://localhost:${port}!`));
+const fs = require(`fs`);
+
+const options = {};
+
+const passphrase = process.env.password;
+if (passphrase) {
+  options.pfx = fs.readFileSync(`./cert`);
+  options.passphrase = passphrase;
+}
+
+const serverType = passphrase ? require(`https`) : require(`http`);
+const server = serverType.createServer(options, app);
+
+server.listen(port, () => console.log(`App is ready at http://localhost:${port}!`));
